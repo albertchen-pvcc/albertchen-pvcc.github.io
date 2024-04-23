5f9c8430e7314697ff0cbe9612ae50957bc621d7
albertchen-pvcc <160061155+albertchen-pvcc@users.noreply.github.com>
2024-04-23 00:47:56 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+# Name: Albert Chen
+# Prog Purpose: Hello World
+
+print('Hello World! This is my first Python Program!')
+print('I can program in HTML, which is awesome!')
+print('However, HTML cannot do arithmetic.')
+print('But Python can do \n\t Add \n\t Subtract \n\t Multiply \n\t Divide')
+print('and much more!!!')
+
+input()
